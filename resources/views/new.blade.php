2efb050f330ee8e41947bc6e32ae73680a535dd7
unknown <nafish@chapter247.com>
2022-09-30 11:21:20 +0530

    some other changes

--- resources/views/new.blade.php
+++ resources/views/new.blade.php
@@ -82,149 +82,150 @@
                     class="fas fa-times"></i> X</button>
               </div>
             </header>
-            <aside class="flex flex-col menu_sidebar" :class="{'hidden sm:flex sm:flex-col': window.outerWidth > 768}">
-              <div class="flex-grow flex flex-col justify-between text-gray-500 bg-gray-800">
-                <nav class="flex flex-col space-y-4">
-                  <a href="#"
-                    class="menu_item active inline-flex items-center py-3 hover:text-gray-400 hover:bg-gray-700 focus:text-gray-400 focus:bg-gray-700 px-2"
-                    :class="{'justify-start': menu, 'justify-center': menu == false}">
-                    <span class="icon"><i class="bi bi-house-door"></i></span>
-                    <span class="ml-3 menu_name" x-show="menu">Overview</span>
-                  </a>
-                  <a href="#" class="menu_item inline-flex items-center py-3 text-blue-600 bg-white px-2"
-                    :class="{'justify-start': menu, 'justify-center': menu == false}">
-                    <span class="icon"><i class="bi bi-bar-chart"></i></span>
-                    <span class="ml-3 menu_name" x-show="menu">Chart</span>
-                  </a>
-                  <a href="#"
-                    class="menu_item inline-flex items-center py-3 hover:text-gray-400 hover:bg-gray-700 focus:text-gray-400 focus:bg-gray-700 px-2"
-                    :class="{'justify-start': menu, 'justify-center': menu == false}">
-                    <span class="icon"><i class="bi bi-eye-slash"></i></span>
-                    <span class="ml-3 menu_name" x-show="menu">Dark Pool</span>
-                  </a>
-                  <a href="#"
-                    class="menu_item inline-flex items-center py-3 hover:text-gray-400 hover:bg-gray-700 focus:text-gray-400 focus:bg-gray-700 px-2"
-                    :class="{'justify-start': menu, 'justify-center': menu == false}">
-                    <span class="icon"><i class="bi bi-exclamation-triangle"></i></span>
-                    <span class="ml-3 menu_name" x-show="menu">Divergences</span>
-                  </a>
-                  <a href="#"
-                    class="menu_item inline-flex items-center py-3 hover:text-gray-400 hover:bg-gray-700 focus:text-gray-400 focus:bg-gray-700 px-2"
-                    :class="{'justify-start': menu, 'justify-center': menu == false}">
-                    <span class="icon"><i class="bi bi-arrow-down-up"></i></span>
-                    <span class="ml-3 menu_name" x-show="menu">Cycle Data</span>
-                  </a>
-                  <a href="#"
-                    class="menu_item inline-flex items-center py-3 hover:text-gray-400 hover:bg-gray-700 focus:text-gray-400 focus:bg-gray-700 px-2"
-                    :class="{'justify-start': menu, 'justify-center': menu == false}">
-                    <span class="icon"><i class="bi bi-file-earmark-text"></i></span>
-                    <span class="ml-3 menu_name" x-show="menu">Pivot Points</span>
-                  </a>
-                  <a href="#"
-                    class="menu_item inline-flex items-center py-3 hover:text-gray-400 hover:bg-gray-700 focus:text-gray-400 focus:bg-gray-700 px-2"
-                    :class="{'justify-start': menu, 'justify-center': menu == false}">
-                    <span class="icon"><i class="bi bi-activity"></i></span>
-                    <span class="ml-3 menu_name" x-show="menu">Cheat Sheet</span>
-                  </a>
-                  <a href="#"
-                    class="menu_item inline-flex items-center py-3 hover:text-gray-400 hover:bg-gray-700 focus:text-gray-400 focus:bg-gray-700 px-2"
-                    :class="{'justify-start': menu, 'justify-center': menu == false}">
-                    <span class="icon"><i class="bi bi-exclamation-triangle"></i></span>
-                    <span class="ml-3 menu_name" x-show="menu">All Trades</span>
-                  </a>
-                  <a href="#"
-                    class="menu_item inline-flex items-center py-3 hover:text-gray-400 hover:bg-gray-700 focus:text-gray-400 focus:bg-gray-700 px-2"
-                    :class="{'justify-start': menu, 'justify-center': menu == false}">
-                    <span class="icon"><i class="bi bi-camera"></i></span>
-                    <span class="ml-3 menu_name" x-show="menu">Snapshots</span>
-                  </a>
-                </nav>
-              </div>
-                <div class="glance_bar" x-show="menu">
-                    <p>Glance quotea</p>
-                    <div class="progress">
-                        <div class="progress-bar" style="width:50%"></div>
+            <section class="body_sec">
+                <aside class="flex flex-col menu_sidebar" :class="{'hidden sm:flex sm:flex-col': window.outerWidth > 768}">
+                  <div class="flex-grow flex flex-col justify-between text-gray-500 bg-gray-800">
+                    <nav class="flex flex-col space-y-4">
+                      <a href="#"
+                        class="menu_item active inline-flex items-center py-3 hover:text-gray-400 hover:bg-gray-700 focus:text-gray-400 focus:bg-gray-700 px-2"
+                        :class="{'justify-start': menu, 'justify-center': menu == false}">
+                        <span class="icon"><i class="bi bi-house-door"></i></span>
+                        <span class="ml-3 menu_name" x-show="menu">Overview</span>
+                      </a>
+                      <a href="#" class="menu_item inline-flex items-center py-3 text-blue-600 bg-white px-2"
+                        :class="{'justify-start': menu, 'justify-center': menu == false}">
+                        <span class="icon"><i class="bi bi-bar-chart"></i></span>
+                        <span class="ml-3 menu_name" x-show="menu">Chart</span>
+                      </a>
+                      <a href="#"
+                        class="menu_item inline-flex items-center py-3 hover:text-gray-400 hover:bg-gray-700 focus:text-gray-400 focus:bg-gray-700 px-2"
+                        :class="{'justify-start': menu, 'justify-center': menu == false}">
+                        <span class="icon"><i class="bi bi-eye-slash"></i></span>
+                        <span class="ml-3 menu_name" x-show="menu">Dark Pool</span>
+                      </a>
+                      <a href="#"
+                        class="menu_item inline-flex items-center py-3 hover:text-gray-400 hover:bg-gray-700 focus:text-gray-400 focus:bg-gray-700 px-2"
+                        :class="{'justify-start': menu, 'justify-center': menu == false}">
+                        <span class="icon"><i class="bi bi-exclamation-triangle"></i></span>
+                        <span class="ml-3 menu_name" x-show="menu">Divergences</span>
+                      </a>
+                      <a href="#"
+                        class="menu_item inline-flex items-center py-3 hover:text-gray-400 hover:bg-gray-700 focus:text-gray-400 focus:bg-gray-700 px-2"
+                        :class="{'justify-start': menu, 'justify-center': menu == false}">
+                        <span class="icon"><i class="bi bi-arrow-down-up"></i></span>
+                        <span class="ml-3 menu_name" x-show="menu">Cycle Data</span>
+                      </a>
+                      <a href="#"
+                        class="menu_item inline-flex items-center py-3 hover:text-gray-400 hover:bg-gray-700 focus:text-gray-400 focus:bg-gray-700 px-2"
+                        :class="{'justify-start': menu, 'justify-center': menu == false}">
+                        <span class="icon"><i class="bi bi-file-earmark-text"></i></span>
+                        <span class="ml-3 menu_name" x-show="menu">Pivot Points</span>
+                      </a>
+                      <a href="#"
+                        class="menu_item inline-flex items-center py-3 hover:text-gray-400 hover:bg-gray-700 focus:text-gray-400 focus:bg-gray-700 px-2"
+                        :class="{'justify-start': menu, 'justify-center': menu == false}">
+                        <span class="icon"><i class="bi bi-activity"></i></span>
+                        <span class="ml-3 menu_name" x-show="menu">Cheat Sheet</span>
+                      </a>
+                      <a href="#"
+                        class="menu_item inline-flex items-center py-3 hover:text-gray-400 hover:bg-gray-700 focus:text-gray-400 focus:bg-gray-700 px-2"
+                        :class="{'justify-start': menu, 'justify-center': menu == false}">
+                        <span class="icon"><i class="bi bi-exclamation-triangle"></i></span>
+                        <span class="ml-3 menu_name" x-show="menu">All Trades</span>
+                      </a>
+                      <a href="#"
+                        class="menu_item inline-flex items-center py-3 hover:text-gray-400 hover:bg-gray-700 focus:text-gray-400 focus:bg-gray-700 px-2"
+                        :class="{'justify-start': menu, 'justify-center': menu == false}">
+                        <span class="icon"><i class="bi bi-camera"></i></span>
+                        <span class="ml-3 menu_name" x-show="menu">Snapshots</span>
+                      </a>
+                    </nav>
+                  </div>
+                    <div class="glance_bar" x-show="menu">
+                        <p>Glance quotea</p>
+                        <div class="progress">
+                            <div class="progress-bar" style="width:50%"></div>
+                        </div>
+                        <p>50 of 100</p>
                     </div>
-                    <p>50 of 100</p>
-                </div>
-            </aside>
-            <div class="right_side_bar flex-grow text-gray-800">
-
-              <main class="">
-                <div class="option-header flex items-center justify-between">
-                    <h5 class="mb-0">Options Flow</h5>
-                    <div class="btn-group">
-                      <button class="btn btn-border">Action 1</button>
-                      <button class="btn btn-bg-color">Action 2</button>
-                      <i class="bi bi-three-dots-vertical ml-2"></i>
+                </aside>
+                <div class="right_side_bar flex-grow text-gray-800">
+                  <main class="">
+                    <div class="option-header flex items-center justify-between">
+                        <h5 class="mb-0">Options Flow</h5>
+                        <div class="btn-group">
+                          <button class="btn btn-border">Action 1</button>
+                          <button class="btn btn-bg-color">Action 2</button>
+                          <i class="bi bi-three-dots-vertical ml-2"></i>
+                        </div>
                     </div>
-                </div>
-                <div class="body_content">
-                    <div class="option_table">
-                        <table class="table rounded-t-lg m-5 w-5/6 mx-auto bg-gray-200 text-gray-800 w-full">
-                          <thead>
-                            <tr>
-                              <th scope="col">Ticker</th>
-                              <th scope="col">Company</th>
-                              <th scope="col">Country</th>
-                              <th scope="col">Market Cap</th>
-                              <th scope="col">Price</th>
-                              <th scope="col">Change</th>
-                              <th scope="col">Volumn</th>
-                            </tr>
-                          </thead>
-                          <tbody>
-                            <tr>
-                              <th scope="row">METX</th>
-                              <td>Meten Holding group Ltd.</td>
-                              <td class="country flex items-center"><img src="{{asset('images/china.png')}}" alt="" /> China</td>
-                              <td>7.73M</td>
-                              <td>$ 0.56</td>
-                              <td><span class="down">-3.50%</span></td>
-                              <td>167619</td>
-                            </tr>
-                            <tr>
-                              <th scope="row">JFU</th>
-                              <td>9F Inc.</td>
-                              <td class="country flex items-center"><img src="{{asset('images/china.png')}}" alt="" />China</td>
-                              <td>95.58M</td>
-                              <td>$ 0.39</td>
-                              <td><span class="down">-4.85%</span></td>
-                              <td>59,237</td>
-                            </tr>
-                            <tr>
-                              <th scope="row">STIX</th>
-                              <td>Semantic Inc.</td>
-                              <td class="country flex items-center"><img src="{{asset('images/brazil.png')}}" alt="" />Brazil</td>
-                              <td>214.73M</td>
-                              <td>$ 2.8</td>
-                              <td><span class="up">9.18%</span></td>
-                              <td>45,229</td>
-                            </tr>
-                            <tr>
-                              <th scope="row">CRXT</th>
-                              <td>Clerus TherapeuticsHolding, Inc.</td>
-                              <td class="country flex items-center"><img src="{{asset('images/usa.png')}}" alt="" />USA</td>
-                              <td>4,15M</td>
-                              <td>$ 0.06</td>
-                              <td><span class="down">-17.20%</span></td>
-                              <td>1,160,042</td>
-                            </tr>
-                            <tr>
-                              <th scope="row">STIX</th>
-                              <td>Semantic Inc.</td>
-                              <td class="country flex items-center"><img src="{{asset('images/brazil.png')}}" alt="" />Brazil</td>
-                              <td>214.73M</td>
-                              <td>$ 2.8</td>
-                              <td><span class="up">9.18%</span></td>
-                              <td>45,229</td>
-                            </tr>
-                          </tbody>
-                        </table> 
+                    <div class="body_content">
+                        <div class="option_table">
+                            <table class="table rounded-t-lg m-5 w-5/6 mx-auto bg-gray-200 text-gray-800 w-full">
+                              <thead>
+                                <tr>
+                                  <th scope="col">Ticker</th>
+                                  <th scope="col">Company</th>
+                                  <th scope="col">Country</th>
+                                  <th scope="col">Market Cap</th>
+                                  <th scope="col">Price</th>
+                                  <th scope="col">Change</th>
+                                  <th scope="col">Volumn</th>
+                                </tr>
+                              </thead>
+                              <tbody>
+                                <tr>
+                                  <th scope="row">METX</th>
+                                  <td>Meten Holding group Ltd.</td>
+                                  <td class="country flex items-center"><img src="{{asset('images/china.png')}}" alt="" /> China</td>
+                                  <td>7.73M</td>
+                                  <td>$ 0.56</td>
+                                  <td><span class="down">-3.50%</span></td>
+                                  <td>167619</td>
+                                </tr>
+                                <tr>
+                                  <th scope="row">JFU</th>
+                                  <td>9F Inc.</td>
+                                  <td class="country flex items-center"><img src="{{asset('images/china.png')}}" alt="" />China</td>
+                                  <td>95.58M</td>
+                                  <td>$ 0.39</td>
+                                  <td><span class="down">-4.85%</span></td>
+                                  <td>59,237</td>
+                                </tr>
+                                <tr>
+                                  <th scope="row">STIX</th>
+                                  <td>Semantic Inc.</td>
+                                  <td class="country flex items-center"><img src="{{asset('images/brazil.png')}}" alt="" />Brazil</td>
+                                  <td>214.73M</td>
+                                  <td>$ 2.8</td>
+                                  <td><span class="up">9.18%</span></td>
+                                  <td>45,229</td>
+                                </tr>
+                                <tr>
+                                  <th scope="row">CRXT</th>
+                                  <td>Clerus TherapeuticsHolding, Inc.</td>
+                                  <td class="country flex items-center"><img src="{{asset('images/usa.png')}}" alt="" />USA</td>
+                                  <td>4,15M</td>
+                                  <td>$ 0.06</td>
+                                  <td><span class="down">-17.20%</span></td>
+                                  <td>1,160,042</td>
+                                </tr>
+                                <tr>
+                                  <th scope="row">STIX</th>
+                                  <td>Semantic Inc.</td>
+                                  <td class="country flex items-center"><img src="{{asset('images/brazil.png')}}" alt="" />Brazil</td>
+                                  <td>214.73M</td>
+                                  <td>$ 2.8</td>
+                                  <td><span class="up">9.18%</span></td>
+                                  <td>45,229</td>
+                                </tr>
+                              </tbody>
+                            </table> 
+                        </div>
                     </div>
+                  </main>
                 </div>
-              </main>
-            </div>
+            </section>
           </div>
 
 
